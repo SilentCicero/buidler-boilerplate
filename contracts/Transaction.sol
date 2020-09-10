e0009976dc2fa022fe52c833c7b76aef264d92fb
SilentCicero <thenickdodson@gmail.com>
2020-09-10 15:41:39 -0400

    fix compiler version

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.5.15;
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.5.15;
 pragma experimental ABIEncoderV2;
 
 library Transaction {
